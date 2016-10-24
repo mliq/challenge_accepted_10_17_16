99400ed98e2edb68498f48fe80675bfb17c32afa
Michael Liquori <mliquori@nerdery.com>
2016-10-23 23:18:51 -0500

    4 tabs, lengthened js file

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 'use strict';
 
 const form = document.querySelector('form');
+const EXPECTED_INPUT = 'front-end';
+const DISPLAY_NONE = 'none';
+const DISPLAY_BLOCK = 'block';
+const ERROR_BORDER = '1px solid #e84f4f';
 
 form.addEventListener('submit', function onSubmit(event) {
     const errorElement = document.getElementsByClassName('input-error')[0];
-    if (this.elements.value.value === 'front-end') {
-        errorElement.style.display = 'none';
+    const inputValue = this.elements.value.value;
+    const inputFieldEl = document.getElementsByClassName('input-field_text')[0];
+
+    if (inputValue === EXPECTED_INPUT) {
+        errorElement.style.display = DISPLAY_NONE;
     } else {
-        const inputFieldEl = document.getElementsByClassName('input-field_text')[0];
-        errorElement.style.display = 'block';
-        inputFieldEl.style.border = '1px solid #e84f4f';
+        errorElement.style.display = DISPLAY_BLOCK;
+        inputFieldEl.style.border = ERROR_BORDER;
         event.preventDefault();
     }
 });
